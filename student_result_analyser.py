a699d2b2722618805a7f72b264f3548fed4ab078
arushkumar-aiml <arushkumarsince2007@gmail.com>
2026-09-16 19:48:37 +0530

    complete

---
+++ student_result_analyser.py
@@ -0,0 +1,11 @@
+n=int(input("enter the marks:-"))
+
+if n<=100 and n >=80:
+    print("grade A")
+elif n<=80 and n>=60:
+    print("grade B")
+if n<=60 and n>=40:
+    print("grade C")
+else:
+    print("fail")
+
